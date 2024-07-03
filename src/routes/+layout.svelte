41a0f2fa39a285f2c7d95288753aa9fb0d4b6e44
mrdiv <mr.divyanshu10@gmail.com>
2024-07-04 03:43:55 +0530

    first push

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    // import type { LayoutData } from './$types';
+    import Navbar from "$lib/Navbar.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import "../app.css";
+    // export let data: LayoutData;
+</script>
+
+<Navbar />
+<slot />
+<Footer />
---
+++ src/routes/[slug]/+page.js
@@ -0,0 +1,4 @@
+
+export async function load ({params}) {
+    return {content: `Hello ${params.slug}`};
+}
