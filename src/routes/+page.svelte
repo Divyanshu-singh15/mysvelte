18dcf074f828dea092910c0a6980ff3e8f247efb
Divyanshu Singh <mr.divyanshu10@gmail.com>
2024-07-08 21:24:39 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Divyanshu's SvelteKit</h1>
+<p>This is the demonstration of [slug] as well as header and footer adding in Svelte and then deploying on Vercel</p>
